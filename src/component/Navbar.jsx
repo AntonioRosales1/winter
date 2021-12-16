1345fffd19ad09327f1cdb66248fcd6f46d1ef93
AntonioRosales1 <elrluis213@gmail.com>
2021-12-16 00:05:19 -0800

    Home added

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Navbar = () => {
     return (
         <div>
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <nav class="navbar navbar-expand-lg navbar-light bg-white py-3 shadow-sm">
   <div class="container">
     <a class="navbar-brand fw-bold fs-4" href="#">FINEST WEAR</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -15,13 +15,26 @@ const Navbar = () => {
           <a class="nav-link active" aria-current="page" href="#">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link disabled">Disabled</a>
+          <a class="nav-link" href='#'>Products</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href='#'>About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href='#'>Contact</a>
         </li>
       </ul>
-      <form class="d-flex">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
+       <div className="buttons">
+           <a href="" className="btn btn-outline-dark">
+             <i className="fa fa-sign-in me-1"></i>  Login
+           </a>
+           <a href="" className="btn btn-outline-dark ms-2">
+             <i className="fa fa-user-plus me-1"></i>  Register
+           </a>
+           <a href="" className="btn btn-outline-dark ms-2">
+             <i className="fa fa-shopping-cart me-1"></i>  cart (0)
+           </a>
+           </div>
     </div>
   </div>
 </nav>
