df54a0063f7005fcc233a17ddd7657e9c7c4ff55
AntonioRosales1 <elrluis213@gmail.com>
2021-12-17 00:22:32 -0800

    Added products

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -3,16 +3,16 @@ import React from "react";
 const Home = () => {
     return (
         <div className='hero'>
-            <div class="card mb-3" style="max-width: 540px;">
-                <div class="row g-0">
-                    <div class="col-md-4">
+            <div className="card mb-3" style="max-width: 540px;">
+                <div className="row g-0">
+                    <div className="col-md-4">
                         <img src="/images/Winter1.png" class="img-fluid rounded-start" alt="..." />
                     </div>
                     <div class="col-md-8">
                         <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                            <h5 className="card-title">Card title</h5>
+                            <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
                         </div>
                     </div>
                 </div>
